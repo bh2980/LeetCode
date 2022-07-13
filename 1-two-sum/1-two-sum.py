40e9f517fec51a28208783008fe5ee3c1cd9737b
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2022-07-13 21:14:34 +0900

    Time: 96 ms (64.14%), Space: 15.1 MB (50.46%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,20 +1,11 @@
-from collections import defaultdict
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        length = len(nums)
-        
-        nums_dict = defaultdict(lambda: list())
-        
-        for i in range(length):
-            nums_dict[nums[i]].append(i)
+        nums_dict = dict()
             
-        nums_dict = dict(sorted(nums_dict.items(), key=lambda x:x[0]))
-            
-        for key, value in nums_dict.items():
-            if target - key in nums_dict:
-                if target - key == target//2:
-                    return value
-                else:
-                    return [value[0], nums_dict[target - key][0]]
+        for index in range(len(nums)):
+            num = nums[index]
+            try:
+                return [nums_dict[target -num], index]
+            except:
+                nums_dict[num] = index
             
